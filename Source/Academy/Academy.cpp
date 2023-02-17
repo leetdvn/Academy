05a47d378b5175384e655ea00160330590280f94
MrLeeTa <111244237+leetdvn@users.noreply.github.com>
2023-02-18 00:55:39 +0700

    Initial commit

---
+++ Source/Academy/Academy.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Academy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Academy, "Academy" );
---
+++ Source/Academy/AcademyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AcademyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACADEMY_API AAcademyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
