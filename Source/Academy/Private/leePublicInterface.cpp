38def3b4d82e34f57f134b3ec360fff98ef5b7aa
MrLeeTa <111244237+leetdvn@users.noreply.github.com>
2023-05-04 21:54:16 +0700

    update

--- Source/Academy/Private/leePublicInterface.cpp
+++ Source/Academy/Private/leePublicInterface.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include <Kismet/BlueprintPlatformLibrary.h>
 #include "leePublicInterface.h"
+#include <Kismet/BlueprintPlatformLibrary.h>
 #include <AssetRegistry/AssetRegistryModule.h>
 #include "HAL/FileManagerGeneric.h"
 #include <Kismet/GameplayStatics.h>
